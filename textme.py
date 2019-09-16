054d442fe7fbb12f7706389f1cf9ba0ee24fef58
amnbkshi <amanbakshi4@gmail.com>
2019-09-16 23:13:00 +0530

    Added function

--- textme.py
+++ textme.py
@@ -1,7 +1,18 @@
-import twilio
+#! python3
+# textme.py - Helper function to send notification to my number.
 
-def textme():
-	
+import os
+from twilio.rest import TwilioRestClient
+
+# Reading account details from environment variable":
+accSID = os.env.get('tw_SID')
+token = os.env.get('tw_token')
+receiverNumber = os.env.get('tw_receiver')
+senderNumber = os.env.get('tw_sender')
+
+def textme(message):
+	twCli = TwilioRestClient(accSID, token)
+	twCli.messages.create(body=message, from_=senderNumber, to=receiverNumber)
 
 
 if __name__ == '__main__':
