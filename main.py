799462e939c9915090ccf0da50e0bfb1dd8d6457
amnbkshi <amanbakshi4@gmail.com>
2019-09-17 15:09:14 +0530

    First commit for main code.

---
+++ main.py
@@ -0,0 +1,17 @@
+import bs4
+import textme
+from datetime import datetime
+from selenium import webdriver
+
+URL = 'https://www.cricbuzz.com/cricket-team/india/2/schedule'
+
+browser = webdriver.Firefox()
+browser.get(URL)
+
+html = browser.page_source
+soup = bs4.BeautifulSoup(html, "html.parser")
+
+element = soup.find('div', 
+            class_ = 'cb-col-100 cb-col cb-series-brdr cb-series-matches ng-scope')
+
+date = element.div.span.text
