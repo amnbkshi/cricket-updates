3c8ff6b0f7c36940d8f113ab4011cfe63119c25f
amnbkshi <amanbakshi4@gmail.com>
2019-09-17 19:33:56 +0530

    Extracted Data.

--- main.py
+++ main.py
@@ -14,6 +14,11 @@ html = browser.page_source
 soup = bs4.BeautifulSoup(html, "html.parser")
 
 element = soup.find('div', 
-            class_ = 'cb-col-100 cb-col cb-series-brdr cb-series-matches ng-scope')
+            class_ = 'cb-col-100 cb-col cb-series-matches ng-scope')
 
-date = element.div.span.text
+date = element.div.span.text #split by - for test matches
+#datetime.strptime(date, '%b %d, %a')
+
+team = element.find('a', class_='text-hvr-underline').span.text
+venue = element.find('div', class_="text-gray cb-ovr-flo").text
+time = element.find('div', class_="cb-font-12 text-gray").select('span + span')[0].text
