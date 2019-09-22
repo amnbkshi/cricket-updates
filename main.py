5938f6e01a6392e3d8566323ca6442d0dea36b0d
amnbkshi <amanbakshi4@gmail.com>
2019-09-22 17:48:19 +0530

    Completed code

--- main.py
+++ main.py
@@ -10,29 +10,31 @@ options = webdriver.FirefoxOptions()
 options.headless = True
 browser = webdriver.Firefox(options=options)
 
-#loop
-browser.get(URL)
-html = browser.page_source
-soup = bs4.BeautifulSoup(html, "html.parser")
-
-element = soup.find('div', 
-            class_ = 'cb-col-100 cb-col cb-series-matches ng-scope')
-
-date = dt.datetime.strptime(element.div.span.text, '%b %d, %a') #split by - for test matches
-team = element.find('a', class_='text-hvr-underline').span.text.strip()
-venue = element.find('div', class_="text-gray cb-ovr-flo").text
-
-time = element.find('div', class_="cb-font-12 text-gray").select('span + span')[0].text.strip()
-ti = dt.datetime.strptime(time, '%I:%M %p')
-
-message = f"Don't miss out {team} today at {time}!"
-
-curr_time = dt.datetime.now()
-send_time = dt.datetime(dt.datetime.now().year, date.month, date.day,
-                     ti.hour, ti.minute)
-
-delta = dt.timedelta(hours=5)
-
-if (send_time - curr_time) < delta:
-    #send message
+while True:
+    time.sleep(7200) #check every 2 hours each day
+    browser.get(URL)
+    html = browser.page_source
+    soup = bs4.BeautifulSoup(html, "html.parser")
     
+    element = soup.find('div', 
+                class_ = 'cb-col-100 cb-col cb-series-matches ng-scope')
+    
+    date = dt.datetime.strptime(element.div.span.text, '%b %d, %a') #split by - for test matches
+    team = element.find('a', class_='text-hvr-underline').span.text.strip()
+    venue = element.find('div', class_="text-gray cb-ovr-flo").text
+    
+    time = element.find('div', class_="cb-font-12 text-gray").select('span + span')[0].text.strip()
+    ti = dt.datetime.strptime(time, '%I:%M %p')
+    
+    message = f"Don't miss out {team} today at {time}!"
+    
+    curr_time = dt.datetime.now()
+    send_time = dt.datetime(dt.datetime.now().year, date.month, date.day,
+                         ti.hour, ti.minute)
+    
+    delta = dt.timedelta(hours=5)
+    
+    if (send_time - curr_time) < delta:
+        #print(message)
+        textme.textme(message)
+        time.sleep(86400) #sleep for a day after and start checking the next day
